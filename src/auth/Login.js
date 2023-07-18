e7dbdf1ed9cac96d367ef642ff214107ece1e356
Scorpkon <aguilajsb@gmail.com>
2023-07-18 14:59:36 -0500

    Autenticacion con redux

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,14 +1,33 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import {login} from '../actions/userActions';
+
 
 export const Login =()=>{
-    const {loginWithRedirect}= useAuth0();
-    function handleLogin(){
-          loginWithRedirect();
+    const {loginWithRedirect, isAuthenticated,user, isLoading}= useAuth0();
+   const dispatch= useDispatch();
+   const handleLogin= async()=>{
+    try {
+        await loginWithRedirect();
+    } catch (error) {
+        console.log(error);
     }
 
+   };
+        useEffect(() => {
+           if(isAuthenticated && user){
+            dispatch(login(user))
+           };
+        },[isAuthenticated, user, dispatch]);
+
+        if (isLoading) {
+            return<div> Cargando...</div>;
+        }
+
+
     return(
-        <button onClick={()=>(handleLogin())}>
+        <button onClick={handleLogin}>
              Acceder
         </button>
     )
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import store from'./store/store'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <Auth0Provider
-      domain='dev-s6o4m30i853tmvtc.us.auth0.com'
-      clientId='ZkLz9nAEsf9oNeUDoOdIcl4bk12Tq4d5'
-      redirectUri={window.location.origin}>
-      <App />
-    </Auth0Provider>
+    <Provider store={store}>
+      <Auth0Provider
+        domain='dev-s6o4m30i853tmvtc.us.auth0.com'
+        clientId='ZkLz9nAEsf9oNeUDoOdIcl4bk12Tq4d5'
+        redirectUri={window.location.origin}>
+        <App />
+      </Auth0Provider>
+    </Provider>
   </React.StrictMode>
 
 );
